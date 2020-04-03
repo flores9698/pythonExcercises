24f77d07f7a5682de55aac9d656a52e16a38a881
Saul <saulflores3126@gmail.com>
2020-04-03 00:09:05 -0600

    Extra current_year is now taken from the datetime module, so it would always be up to date

--- ex1.py
+++ ex1.py
@@ -2,9 +2,14 @@
 """ Create a program that asks the user to enter their name and their age. 
 Print out a message addressed to them that tells them the year that they will turn 100 years old."""
 
+import datetime
+
+
+datetime.date.today().strftime("%Y")
+
 def yearOF100(age):
 	"""Returns the year when the person turn 100 year """
-	current_year= 2020
+	current_year= int(datetime.date.today().strftime("%Y"))
 	year100= current_year-age+100
 
 	return year100
